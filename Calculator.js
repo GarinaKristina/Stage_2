f1c1cd4c8c47219e728f8b46e5f90c542c84f1b3
GarinaKristina <87827778+GarinaKristina@users.noreply.github.com>
2022-01-19 14:27:16 +0200

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -1,17 +1,21 @@
 class Calculator { 
 
     add(a, b) {
+        this.validateParameter(a, b);
     return a + b;
     }
+    
 
     multiply(a, b) {
-        return a * b;
+        this.validateParameter(a, b);
+    return a * b;
     }
-    validateParametr(a, b) {
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('You write not a number, you should change symbol');
 
-        }
+    validateParameter(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('You write not a number, you should change symbol');
+
+    }
     }
 }
 module.exports = Calculator;
