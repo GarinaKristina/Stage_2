55d6b9cb1fb633ce89b7bc8b9fe56c8accd25969
GarinaKristina <87827778+GarinaKristina@users.noreply.github.com>
2022-01-19 14:33:15 +0200

    Add files via upload

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,28 +1,28 @@
 const { expect } = require('chai');
-//const Calculator = require('/IT programm/app/Calculator.js');
-//const Calculator = require('/IT programm/app/Calculator.js');
 const Calculator = require('/IT programm/app/Calculator.js');
 
-describe("Calculator method add", () => { 
+describe('Calculator method', () => {
+  const calc = new Calculator();
 
-    const calc = new Calculator;
-
+  describe('Method add', () => {
     it('Should return the sum of two numbers', () => {
-expect(calc.add(5, 8)).to.equal(13);
+      expect(calc.add(5, 8)).to.equal(13);
     });
-//});
 
-describe("Calculator method multiply", () => {
+    it('Should trow error if symbol is not a number', () => {
+      const addWithError = () => calc.add('cat', 4);
+      expect(addWithError).to.throw(Error);
+    });
+  });
 
-it('Should return the sign of multiplication of two numbers', () => {    
-expect(calc.multiply(4, 4)).to.equal(16);
-});
-});
+  describe('Method multiply', () => {
+    it('Should return the value of multiplication of two numbers', () => {
+      expect(calc.multiply(4, 4)).to.equal(16);
+    });
 
-it("Should trow error if symbol is not a number", () => {
-const addWithError =() => calc.add('cat', 4);
-expect(addWithError).to.throw(Error);
+    it('Should trow error if symbol is not a number', () => {
+      const multiplyWithError = () => calc.multiply('dog', 4);
+      expect(multiplyWithError).to.throw(Error);
+    });
+  });
 });
-});
-
-//console.log(new Calculator().add(100, 200));
